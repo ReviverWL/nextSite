76e903619bfa60b895b9ef745204022f2ccc28f6
ReviverWL <hardssd@mail.ru>
2020-04-28 09:59:24 +0300

    Коммитим новый проект

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='my-header'>
+        <img src='https://say-hi.me/wp-content/uploads/2018/07/mozilla-logos-1.png' alt="none" />
+    </header>
+};
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='my-content'>
+        <div className='statbar'>
+            <img src='https://lh3.googleusercontent.com/proxy/HF5ES4Hi4zXr25vtFfi3HzfuYQi9X5BYNCCQ0pvJpbegSb4UYcqoiGpYc0QxRypMCdbfisBliIhYpw7giH9l7IHW5F7VitiZk6IpLxfn1LW35U01RygLheqMqMpL9mg5' alt='nano'/>
+        </div>
+        <div className='ava'>
+            <img src='https://sun9-20.userapi.com/c824201/v824201969/173425/UZNGRozhtic.jpg?ava=1' alt='nani'/>
+        </div>
+        <div className='info'></div>
+        <div className='myposts'></div>
+        <div className='button'></div>
+        <div className='message'></div>
+    </div>
+};
+export default Profile;
