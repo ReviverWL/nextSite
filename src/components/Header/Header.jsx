4279fe8445a9477d117e4d944b6e0f212b3a864a
ReviverWL <hardssd@mail.ru>
2020-06-29 14:33:32 +0400

    Всплывашка для отображения (копирования) почты

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import style from './Header.module.css';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = (props) => {
-
+    const[additionalDataVisibility, setAdditionalDataVisibility] = useState(style.hideAdditionalUserData)
     return (
         <header className={style.myHeader}>
             <div className={style.divider}>
                 <img src='https://say-hi.me/wp-content/uploads/2018/07/mozilla-logos-1.png' alt="none" />
                 <div className={style.currentUser}>
-                    {props.authStatus
-                    ?<>
-                        <div>{props.login}<span className={style.quitButton} onClick={props.logout}>выход</span></div>
-                        <div>{props.email}</div>
-                    </>
+                    {props.isAuthUser
+                    ?<div className={style.authUserInfo}>
+                        <div onMouseOver={()=>{setAdditionalDataVisibility(style.showAdditionalUserData)}}
+                             onMouseOut={()=>{setAdditionalDataVisibility(style.hideAdditionalUserData)}}>
+                            {props.authLogin}
+                        </div>
+                        <span className={additionalDataVisibility}
+                        onMouseOver={()=>{setAdditionalDataVisibility(style.showAdditionalUserData)}}
+                        onMouseOut={()=>{setAdditionalDataVisibility(style.hideAdditionalUserData)}}
+                        >{props.email}</span>
+                        <div><span className={style.quitButton} onClick={props.logout}>выход</span></div>
+                    </div>
                     :<NavLink to='/authentification'>логин</NavLink>}
                 </div>
             </div>
